3ff567b618c93cd11491a8060faec2b6c986130d
DKkr <dduk02@gmail.com>
2018-05-30 21:18:43 +0900

    modal 추가

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {Profile} from "../../interfaces/profile";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  profile: Profile = null;
+  constructor(public navCtrl: NavController,
+              public modalCtrl: ModalController) {
 
   }
 
+  presentModalPage(){
+    let modal = this.modalCtrl.create('ModalPage');
+    modal.onDidDismiss((profile) => {
+      if(profile != undefined && Object.keys(profile).length != 0){
+        this.profile = profile;
+      }
+
+      console.log(this.profile);
+    });
+    modal.present();
+  }
 }
